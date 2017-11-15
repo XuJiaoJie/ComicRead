0c044e8951bbd914a5e894dd45793597b4dcc03a
xjhaobang <912091618@qq.com>
2017-11-15 23:03:19 +0800

    解决滑动卡顿问题

--- app/src/main/java/com/xjhaobang/comicread/ui/ComicItemActivity.java
+++ app/src/main/java/com/xjhaobang/comicread/ui/ComicItemActivity.java
@@ -2,7 +2,6 @@ package com.xjhaobang.comicread.ui;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -43,8 +42,6 @@ public class ComicItemActivity extends BaseToolbarActivity implements GetComicIt
     TextView mTvSummary;
     @BindView(R.id.rv_episode)
     RecyclerView mRvEpisode;
-    @BindView(R.id.scroll_view)
-    NestedScrollView mScrollView;
 
     private String mUrl;
     private String mTitle;
